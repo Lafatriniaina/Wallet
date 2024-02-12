b0c638df196f8822e224f9f63782bd92468a26f3
Lafatriniaina <lafatraniaina8@gmail.com>
2024-02-12 22:08:14 +0100

    Icon categorization

---
+++ Frontend/src/types/index.ts
@@ -0,0 +1,41 @@
+import { 
+    MdOutlineSportsBar, 
+    MdCoffee,
+    MdShoppingCart,
+    MdDinnerDining,
+    MdOutlineSmokingRooms,
+    MdWifi,
+    MdPhoneEnabled,
+    MdOutlineWaterDrop,
+    MdDriveEta,
+    MdShoppingBag,
+    MdOutlineSportsEsports,
+    MdCake,
+    MdMedicalServices,
+    MdMonitor,
+    MdAttachMoney,
+    MdFacebook,
+    MdBusinessCenter
+} from "react-icons/md";
+import { IoLogoTiktok } from "react-icons/io5";
+import { SiNetflix } from "react-icons/si";
+
+export type IconTypes = typeof MdOutlineSportsBar 
+    | typeof MdCoffee 
+    | typeof MdShoppingCart 
+    | typeof MdDinnerDining 
+    | typeof MdOutlineSmokingRooms 
+    | typeof MdWifi 
+    | typeof MdPhoneEnabled 
+    | typeof MdOutlineWaterDrop 
+    | typeof MdDriveEta 
+    | typeof MdShoppingBag 
+    | typeof MdOutlineSportsEsports 
+    | typeof MdCake 
+    | typeof MdMedicalServices 
+    | typeof MdMonitor 
+    | typeof MdAttachMoney 
+    | typeof MdFacebook 
+    | typeof IoLogoTiktok 
+    | typeof MdBusinessCenter 
+    | typeof SiNetflix;
