8737e27230dd090a31a5f7c1f07f6180971cab5a
Lafatriniaina <lafatraniaina8@gmail.com>
2024-02-24 22:06:11 +0100

    Energize values from the backend

--- Frontend/src/api/TransactionsApi.ts
+++ Frontend/src/api/TransactionsApi.ts
@@ -22,3 +22,14 @@ export const FetchTopLosers = (user_id: number): Promise<any> => {
         })
         .catch(err => console.error("Erreur lors de la récupération des meilleurs perdants : ", err));
 }
+
+export const LatestTransactionsLists = (user_id: number): Promise<any> => {
+    return fetch(`http://localhost:8000/transactions/latest_transactions/?user_id=${user_id}`)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error('Error to fetch transaction');
+            }
+        })
+}
